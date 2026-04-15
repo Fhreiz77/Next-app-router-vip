9b6bea34d9ac18812675442612aeca9da0877b9c
Fhreiz <iqbalzia2425@gmail.com>
2026-04-15 20:43:17 +0700

    fix proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,18 +1,19 @@
-
 import withAuth from "./proxy/withAuth";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function proxy(request: NextRequest) {
+function mainProxy(request: NextRequest) {
   return NextResponse.next();
 }
 
-export default withAuth(proxy, [
+export const proxy = withAuth(mainProxy, [
   "/dashboard",
   "/profile",
   "/login",
   "/register",
 ]);
+
+export default proxy;
 
 export const config = {
   matcher: ["/dashboard/:path*", "/profile/:path*", "/login", "/register"],
